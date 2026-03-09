2d8500d187c7eebd671dcd18d0f9e0996840b142
Karantiger <pcmtechnology9795@gmail.com>
2026-03-09 20:49:14 +0530

    Portfolio

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/my-portfolio',
+  assetPrefix: '/my-portfolio/',
   eslint: {
     ignoreDuringBuilds: true,
   },
